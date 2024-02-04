d0362603470d2c627466bb74c05c7c0c5c782d80
RodoPZ <34044966+RodoPZ@users.noreply.github.com>
2024-02-04 13:13:09 -0600

    Fix logos

---
+++ logos/arrow-derecha.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps(["color"]);
+</script>
+
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 12 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_152_3630)">
+      <path
+        d="M0.225494 18.33L1.96079 20.0653L11.7647 10.2613L1.96079 0.457428L0.225494 2.19272L8.29412 10.2613L0.225494 18.33Z"
+        :fill="color"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_152_3630">
+        <rect
+          width="11.7647"
+          height="20"
+          fill="white"
+          transform="translate(0 0.261353)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
---
+++ logos/arrow-izquierda.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps(["color"]);
+</script>
+
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 14 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_152_3631)">
+      <path
+        d="M12.6373 2.29077L10.8922 0.555481L1.19608 10.2614L10.902 19.9672L12.6373 18.2319L4.66666 10.2614L12.6373 2.29077Z"
+        :fill="color"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_152_3631">
+        <rect
+          width="12.3529"
+          height="20"
+          fill="white"
+          transform="translate(0.764709 0.261353)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
