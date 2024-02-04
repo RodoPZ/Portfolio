6c8d30405ce8430c0ea47f723e943260af4baab8
RodoPZ <rpinedo2799@gmail.com>
2024-02-04 13:07:41 -0600

    Move to public

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
   plugins: [
     vue(),
     VueI18nPlugin({
